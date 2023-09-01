1e3ae58e9a45d34db48b81580114c6b46df4d474
Naad Dantale <109949049+last-brain-cell@users.noreply.github.com>
2023-09-01 06:20:59 +0530

    minor changes

--- item_publisher.py
+++ item_publisher.py
@@ -2,7 +2,7 @@ import paho.mqtt.client as mqtt
 from random import randint
 import json
 
-# Random Items Data for Random Picking
+# Random Items Data for Random Picking for rudimentry testing - Temporary arrangement for now, in reality, the data will be read from a csv for priliminary testing
 product_names = ['Screws', 'Hammers', 'Pliers', 'Wrenches', 'Nuts', 'Bolts', 'Drills', 'Saws', 'Rivets', 'Chisels', 'Sockets', 'Tapes', 'Soldering Irons', 'Files', 'Tweezers', 'Gloves', 'Measuring Tapes', 'Cutting Discs', 'Welding Masks', 'Clamps']
 product_ids = [4521, 8792, 1234, 5678, 9012, 3456, 7890, 2345, 6789, 8901, 7362, 5789, 9154, 2673, 8291, 5032, 1498, 3826, 6715, 2940]
 rfid_tag_ids = ['RFID123456', 'RFID987654', 'RFID234567', 'RFID876543', 'RFID345678', 'RFID765432', 'RFID456789', 'RFID654321', 'RFID567890', 'RFID098765', 'RFID736251', 'RFID578912', 'RFID915437', 'RFID267385', 'RFID829146', 'RFID503299', 'RFID149865', 'RFID382634', 'RFID671599', 'RFID294087']
@@ -22,6 +22,7 @@ client.on_connect = on_connect
 
 broker_address = "mqtt.eclipseprojects.io"
 client.connect(broker_address)
+
 
 def send_item_data():
     index = randint(0, item_flag - 1)  # Corrected index generation
--- subscribe_process_publish.py
+++ subscribe_process_publish.py
@@ -6,7 +6,8 @@ import pandas as pd
 location_received = False
 current_location = None  # currently scanned location
 
-db_client = MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000")
+db_client = MongoClient("mongodb://127.0.0.1:27017/")  # if this doesn't work comment this one and uncomment the one below
+# db_client = MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000")
 
 def process(message):
     data = json.loads(message)
@@ -28,8 +29,10 @@ def process(message):
 
 
 def append_to_database(data):
-    db = db_client["naad's_db"]
-    collection = db["rfid_data"]
+    db = db_client["Product_Management"]
+    # db = db_client["naad's_db"]
+    collection = db["products"]
+    # collection = db["rfid_data"]
 
     collection.insert_one(data)
     print("Appended to database")
@@ -52,7 +55,7 @@ def append_to_csv(data):
 def on_connect(client, userdata, flags, rc):
     global location_received
     if rc == 0:
-        print("Connected to MQTT broker\nlistening for info...")
+        print("Connected to MQTT broker\nScan a location before scanning an item\nlistening for info...")
         if location_received:
             client.subscribe('item')
             print("Subscribed to 'item' topic")
@@ -60,8 +63,6 @@ def on_connect(client, userdata, flags, rc):
             location_received = True
             client.subscribe("location")
             print("Subscribed to 'location' topic")
-        # client.subscribe("location")
-        # client.subscribe('item')
     else:
         print("Connection failed")
 
